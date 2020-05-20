a80f8d326e1a955dce4017f97babd872835bc12a
Jan <janreimone@gmail.com>
2020-05-21 01:18:13 +0200

    added test support impl

--- sources/cucumber.example/src/test/java/net/reimone/cucumber/example/TestSupport.java
+++ sources/cucumber.example/src/test/java/net/reimone/cucumber/example/TestSupport.java
@@ -3,29 +3,34 @@ package net.reimone.cucumber.example;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.reimone.cucumber.example.pages.GooglePage;
 
 public class TestSupport {
 
+	private GooglePage googlePage = new GooglePage();
+	
 	@Given("I launch Chrome browser")
 	public void i_launch_Chrome_browser() {
-		System.out.println("TestSupport.i_launch_Chrome_browser()");
+		googlePage.launchBrowser();
 	}
 
 	@When("I open Google Homepage")
 	public void i_open_Google_Homepage() {
-		System.out.println("TestSupport.i_open_Google_Homepage()");
+		googlePage.openGoogleURL();
 	}
+	
 	@Then("I verify that the page displays search text box")
 	public void i_verify_that_the_page_displays_search_text_box() {
-		System.out.println("TestSupport.i_verify_that_the_page_displays_search_text_box()");
+		googlePage.checkSearchBoxIsDisplayed();
 	}
+	
 	@Then("the page displays Google Search button")
 	public void the_page_displays_Google_Search_button() {
-		System.out.println("TestSupport.the_page_displays_Google_Search_button()");
+		googlePage.checkGoogleSearchButtonIsDisplayed();
 	}
 	
 	@Then("the page displays Im Feeling Lucky button")
 	public void the_page_displays_Im_Feeling_Lucky_button() {
-		System.out.println("TestSupport.the_page_displays_Im_Feeling_Lucky_button()");
+		googlePage.checkImFeelingLuckyButtonIsDisplayed();
 	}
 }
--- sources/cucumber.example/src/test/java/net/reimone/cucumber/example/pages/GooglePage.java
+++ sources/cucumber.example/src/test/java/net/reimone/cucumber/example/pages/GooglePage.java
@@ -1,5 +1,42 @@
 package net.reimone.cucumber.example.pages;
 
-public class GooglePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import net.reimone.cucumber.example.BasePage;
+
+public class GooglePage extends BasePage {
+
+	
+	public void launchBrowser() {
+		driver = new ChromeDriver();
+	}
+	
+	public void openGoogleURL() {
+		driver.get("https://www.google.com");
+	}
+		
+	public void checkSearchBoxIsDisplayed() {
+		if(driver.findElement(By.name("q")).isDisplayed()) {
+			System.out.println("Search text box is displayed");
+		} else {
+			System.out.println("Search text box is NOT displayed");
+		}
+	}
+	
+	public void checkGoogleSearchButtonIsDisplayed() {
+		if(driver.findElement(By.name("btnK")).isDisplayed()) {
+			System.out.println("Google Search button is displayed");
+		} else {
+			System.out.println("Google Search button is NOT displayed");
+		}
+	}
+	
+	public void checkImFeelingLuckyButtonIsDisplayed() {
+		if(driver.findElement(By.name("btnI")).isDisplayed()) {
+			System.out.println("I'm Feeling Lucky button is displayed");
+		} else {
+			System.out.println("I'm Feeling Lucky button is NOT displayed");
+		}
+	}
 }
